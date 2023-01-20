d03168bf235be3fdcfa24149a7c327b3213e109e
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-20 14:10:15 +0700

    Update anime-gremory.js

--- plugins/anime-gremory.js
+++ plugins/anime-gremory.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://revita.herokuapp.com/api/wallpaper/gremory?apikey=ApiRevita'
+	let url = 'https://api.ibeng.tech/api/wallpaper/gremory?apikey=ibeng'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
 }
 handler.command = /^(gremory)$/i

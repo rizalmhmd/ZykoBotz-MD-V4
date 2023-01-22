732b0f132f4f334c6fdae5687f2b6644dc23c34c
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-22 14:16:03 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -5,12 +5,13 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
-  let res = await fetch(`https://api.zeeoneofc.xyz/api/downloader/tiktok?url=${text}&apikey=uNdFM7fR`)
-  let res2 = await res.json()
-  let x = res2.result
+let url = await fetch(`https://saipulanuar.ga/api/download/tiktok2?url=${args[0]}`)
+  let json = await url.json()
+    if (url.status !== 200) throw await url.text()
+    if (!json.status) throw json
   await m.reply(`*_${global.md}_*`)
 let cap = `${ucapan()}\n\nNih ${name}\nCara Simpan\n\n1. Download Dulu Vidio Nya Kalau Sudah\n2. Masuk Ke Dalam Vidio Nya\n3. Lalu Tekan Titik3 Pojok Kanan Atas\n4. Lalu Pilih Yang Ada Bacaan  Simpan `
-conn.sendFile(m.chat, x.nowm, 'tiktok.mp4', cap, m)
+conn.sendFile(m.chat, json.result.video.link1, 'tiktok.mp4', cap, m)
 }
 handler.tags = ['downloader']
 handler.command = /^(downloadertiktok)$/i

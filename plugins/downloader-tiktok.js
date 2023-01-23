43885902d86208a4d677131b6bb02984ca58c4cd
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-23 08:59:49 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,17 +1,16 @@
 import fetch from 'node-fetch'
 import fs from 'fs'
 import moment from 'moment-timezone'
-let handler = async (m, { conn, usedPrefix, command, text }) => {
+let handler = async (m, { conn, usedPrefix, command, args }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
-let url = await fetch(`https://saipulanuar.ga/api/download/tiktok2?url=${args[0]}`)
-  let json = await url.json()
-    if (url.status !== 200) throw await url.text()
-    if (!json.status) throw json
+let res = await fetch(`https://api.ibeng.tech/api/download/tiktok?url=${args[0]}&apikey=ibeng`)
+  if (!res.ok) throw await res.text()
+let json = await res.json()
   await m.reply(`*_${global.md}_*`)
 let cap = `${ucapan()}\n\nNih ${name}\nCara Simpan\n\n1. Download Dulu Vidio Nya Kalau Sudah\n2. Masuk Ke Dalam Vidio Nya\n3. Lalu Tekan Titik3 Pojok Kanan Atas\n4. Lalu Pilih Yang Ada Bacaan  Simpan `
-conn.sendFile(m.chat, json.result.video.link1, 'tiktok.mp4', cap, m)
+conn.sendFile(m.chat, json.result.video, 'tiktok.mp4', cap, m)
 }
 handler.tags = ['downloader']
 handler.command = /^(downloadertiktok)$/i
@@ -35,3 +34,4 @@ function ucapan() {
   }
   return res
 }
+
